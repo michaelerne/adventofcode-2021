94b4839b1ad89990f0c6e94340aaa9cac3d69393
Michael Erne <michael.erne@gmail.com>
2021-12-02 06:06:15 +0100

    day02

--- day02.py
+++ day02.py
@@ -2,11 +2,46 @@ from aocd import get_data, submit
 
 
 def part_a(data):
-    return ''
+    lines = data.split('\n')
+    instructions = [line.split(' ') for line in lines]
+    instructions = [(x, int(y)) for x, y in instructions]
+
+    depth = 0
+    horizontal = 0
+
+    for instruction, amount in instructions:
+        match instruction:
+            case "forward":
+                horizontal += amount
+            case "down":
+                depth += amount
+            case "up": depth -= amount
+
+
+    return depth * horizontal
 
 
 def part_b(data):
-    return ''
+    lines = data.split('\n')
+    instructions = [line.split(' ') for line in lines]
+    instructions = [(x, int(y)) for x, y in instructions]
+
+    aim = 0
+    depth = 0
+    horizontal = 0
+
+    for instruction, amount in instructions:
+        match instruction:
+            case "forward":
+                horizontal += amount
+                depth += aim * amount
+            case "down":
+                aim += amount
+            case "up":
+                aim -= amount
+
+    return depth * horizontal
+
 
 def main():
     data = get_data()
