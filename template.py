81c47773ea757cc36af0eeb408371948084f19b5
Michael Erne <michael.erne@gmail.com>
2021-12-06 06:16:32 +0100

    fix template

--- template.py
+++ template.py
@@ -7,22 +7,24 @@ def part_a(data):
 
 
 def part_b(data):
-    return 'None'
+    return ''
 
 
 def main():
     data = get_data()
 
     example_data = """"""
+    example_solution_a = None
+    example_solution_b = None
 
     example_answer_a = part_a(example_data)
-    assert example_answer_a, None
+    assert example_answer_a == example_solution_a, "example_data did not match for part_a"
 
     answer_a = part_a(data)
     submit(answer=answer_a, part="a")
 
     example_answer_b = part_b(example_data)
-    assert example_answer_b, None
+    assert example_answer_b == example_solution_b, "example_data did not match for part_b"
 
     answer_b = part_b(data)
     submit(answer=answer_b, part="b")
