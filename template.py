d3152b9c251c2d0831c036c8c371d7f14f56cf67
Michael Erne <michael.erne@gmail.com>
2021-12-07 07:35:28 +0100

    update template

--- template.py
+++ template.py
@@ -4,29 +4,29 @@ import parse
 
 def part_a(data):
     input = [int(x) for x in data.split(',')]
-    return ''
+    return 0
 
 
 def part_b(data):
     input = [int(x) for x in data.split(',')]
-    return ''
+    return 0
 
 
 def main():
     data = get_data()
 
     example_data = """"""
-    example_solution_a = None
-    example_solution_b = None
+    example_solution_a = 1
+    example_solution_b = 1
 
     example_answer_a = part_a(example_data)
-    assert example_answer_a == example_solution_a, f"example_data did not match for part_a: {example_solution_a} != {example_answer_a}"
+    assert example_answer_a == example_solution_a, f"example_data did not match for part_a: {example_answer_a} != {example_solution_a}"
 
     answer_a = part_a(data)
     submit(answer=answer_a, part="a")
 
     example_answer_b = part_b(example_data)
-    assert example_answer_b == example_solution_b, f"example_data did not match for part_b: {example_solution_b} != {example_answer_b}"
+    assert example_answer_b == example_solution_b, f"example_data did not match for part_b: {example_answer_b} != {example_solution_b}"
 
     answer_b = part_b(data)
     submit(answer=answer_b, part="b")
