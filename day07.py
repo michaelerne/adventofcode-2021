f0446e4353e871f379514b3c311aac9d5b6511ad
Michael Erne <michael.erne@gmail.com>
2021-12-07 06:35:24 +0100

    hacky day07

---
+++ day07.py
@@ -0,0 +1,52 @@
+from functools import cache
+
+from aocd import get_data, submit
+
+
+def part_a(data):
+    positions = [int(x) for x in data.strip().split(',')]
+
+    cost_fn = lambda x: x
+
+    costs = []
+    for y in range(min(positions), max(positions) + 1):
+        costs.append(sum([cost_fn(abs(x - y)) for x in positions]))
+
+    return min(costs)
+
+
+def part_b(data):
+    positions = [int(x) for x in data.strip().split(',')]
+
+    @cache
+    def cost_fn(d):
+        return d * (d + 1) // 2
+
+    costs = []
+    for y in range(min(positions), max(positions) + 1):
+        costs.append(sum([cost_fn(abs(x - y)) for x in positions]))
+    return min(costs)
+
+
+def main():
+    data = get_data()
+
+    example_data = """16,1,2,0,4,2,7,1,2,14"""
+    example_solution_a = 37
+    example_solution_b = 168
+
+    example_answer_a = part_a(example_data)
+    assert example_answer_a == example_solution_a, "example_data did not match for part_a"
+
+    answer_a = part_a(data)
+    submit(answer=answer_a, part="a")
+
+    example_answer_b = part_b(example_data)
+    assert example_answer_b == example_solution_b, "example_data did not match for part_b"
+
+    answer_b = part_b(data)
+    submit(answer=answer_b, part="b")
+
+
+if __name__ == '__main__':
+    main()
