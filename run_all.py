0c999cfc4c8f814c72f8f7e09259055e85067111
Michael Erne <michael.erne@gmail.com>
2021-12-18 08:35:05 +0100

    tweak run_all

--- run_all.py
+++ run_all.py
@@ -1,20 +1,36 @@
 import importlib
+import time
+from tqdm import tqdm
 
 from aocd import get_data, submit
 
+days_solved = 18
 
 def main():
-    for day in range(1, 18):
-        mod = importlib.import_module(f'day{str(day).zfill(2)}')
+
+    timings = {}
+    for day in tqdm(range(1, days_solved + 1), desc="Solving AoC2021", unit="day"):
+        d = importlib.import_module(f'day{str(day).zfill(2)}')
 
         data = get_data(day=day)
-        print(f"solving day {day}")
-        answer_a = mod.part_a(data)
-        answer_b = mod.part_b(data)
-        print(f"submitting day {day}")
-        submit(answer_a, day=day, part='a')
-        submit(answer_b, day=day, part='b')
 
+        start = time.perf_counter_ns()
+        answer_a = d.part_a(data)
+        end = time.perf_counter_ns()
+        timings[f'day{str(day).zfill(2)}_a'] = end - start
+
+        start = time.perf_counter_ns()
+        answer_b = d.part_b(data)
+        end = time.perf_counter_ns()
+        timings[f'day{str(day).zfill(2)}_b'] = end - start
+
+        submit(answer_a, day=day, part='a', quiet=True, reopen=False)
+        submit(answer_b, day=day, part='b', quiet=True, reopen=False)
+
+    print()
+    print("timings:")
+    for title, duration_ns in timings.items():
+        print(f'{title}: {duration_ns/1E6} ms')
 
 if __name__ == '__main__':
     main()
